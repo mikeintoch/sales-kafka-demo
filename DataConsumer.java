01a44329ec6009d30c7887ee771ddec4dadc052d
Miguel Delgadillo <jdelgadilloh87@gmail.com>
2021-03-31 17:07:56 -0600

    Adding consumer.properties

--- DataConsumer.java
+++ DataConsumer.java
@@ -1,8 +1,5 @@
 // camel-k: language=java property-file=application.properties
 // camel-k: dependency=camel:gson
-// camel-k: dependency=mvn:org.postgresql:postgresql:jar:42.2.13 
-// camel-k: dependency=mvn:org.apache.commons:commons-dbcp2:jar:2.7.0
-// camel-k: dependency=camel:jdbc 
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
