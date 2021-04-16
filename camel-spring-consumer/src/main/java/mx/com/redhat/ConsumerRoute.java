16c98b8c89efc86c36dacaf02d9218ca28dedddb
Miguel Delgadillo <jdelgadilloh87@gmail.com>
2021-04-16 13:07:56 -0500

    Remove unused importa libraries

--- camel-spring-consumer/src/main/java/mx/com/redhat/ConsumerRoute.java
+++ camel-spring-consumer/src/main/java/mx/com/redhat/ConsumerRoute.java
@@ -1,13 +1,7 @@
 package mx.com.redhat;
 
-import javax.sql.DataSource;
-
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.apache.camel.support.DefaultRegistry;
 
 
 @Component
@@ -22,7 +16,6 @@ public class ConsumerRoute extends RouteBuilder {
     
         // This is the actual route
         from("kafka:{{kafka.consumer.topic}}?brokers={{kafka.brokers}}&valueDeserializer={{deserializerClass}}"+registryconfigAvro)
- //       .process(processDB)
         .setBody(simple("INSERT INTO sales(ORDERNUMBER,ORDERDATE,STATUS,CUSTOMERNAME,DEALSIZE,AMOUNT,PRODUCTLINE) values ('${body.getOrderNumber}', '${body.getOrderDate}'" +
                 ", '${body.getStatus}', '${body.getCustomerName}', '${body.getDealSize}', '${body.getAmount}', '${body.getProductline}')"))
         .to("jdbc:dataSource")
